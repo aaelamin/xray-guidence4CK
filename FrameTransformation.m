008359201012ad2b794d858d7ef75d98ae8baa67
Adam Elamin <116977132+aaelamin@users.noreply.github.com>
2023-10-16 17:09:10 -0400

    finished question one

--- FrameTransformation.m
+++ FrameTransformation.m
@@ -9,19 +9,26 @@ function [transformation_A, transformation_B] = FrameTransformation()
 %   Transformation_A: A 4x4 homogeneous transformation matrix detector A.
 %   Transformation_B: A 4x4 homogeneous transformation matrix detector B.
 %
+    
+    angle_A = deg2rad(45);
+    angle_B = deg2rad(-45);
 
-    % Since the origin of centre of gravity in both images of detector A
-    % and detector B is at (0, 0, 0) I assumed that the transformation
-    % frame will be purly rotational
-    transformation_A = [cos(angle_A), -sin(angle_A), 0, 0;
+    translationVector = [0, 0, 0, 1];
+
+    translationMatrix = eye(4); 
+    translationMatrix(1:4, 4) = -translationVector;
+    rotationMatrix_A = [cos(angle_A), -sin(angle_A), 0, 0;
                             sin(angle_A), cos(angle_A), 0, 0;
                             0, 0, 1, 0;
                             0, 0, 0, 1];
 
     
-    transformation_B = [cos(angle_B), -sin(angle_B), 0, 0;
+    rotationMatrix_B = [cos(angle_B), -sin(angle_B), 0, 0;
                             sin(angle_B), cos(angle_B), 0, 0;
                             0, 0, 1, 0;
                             0, 0, 0, 1];
+
+    transformation_A = translationMatrix * rotationMatrix_A;
+    transformation_B = translationMatrix * rotationMatrix_B;
     
   
