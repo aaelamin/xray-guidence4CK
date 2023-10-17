28c6102b495877de86b05cc000205e730706e376
Adam Elamin <116977132+aaelamin@users.noreply.github.com>
2023-10-17 16:17:54 -0400

    finished question 1 and 2

--- FrameTransformation.m
+++ FrameTransformation.m
@@ -9,24 +9,46 @@ function [transformation_A, transformation_B] = FrameTransformation()
 %   Transformation_A: A 4x4 homogeneous transformation matrix detector A.
 %   Transformation_B: A 4x4 homogeneous transformation matrix detector B.
 %
-    
+    % Firstly, we calculate the angles detector A and B were rotated by.
     angle_A = deg2rad(45);
     angle_B = deg2rad(-45);
+    
+    % a 4x4 homogeneous translation matrix has a the form of [1, 0, 0, -ovx
+    %                                                         0, 1, 0, -ovy 
+    %                                                         0, 0, 1, -ovz 
+    %                                                         0, 0, 0, 1 ]
+    % where ovx, ovy, ovx represent the origin of the detination frame we
+    % want to translate to. 
+    % We know the destination frame (detector A and B) has a centre or orgin
+    % (Ov) of (0, 0, 0) so we sub in those values into the 4x4 homogeneous
+    % translation matrix to obtttain matrix below
 
-    translationVector = [0, 0, 0, 1];
+    translationMatrix = [1, 0, 0, 0;
+                         0, 1, 0, 0;
+                         0, 0, 1, 0;
+                         0, 0, 0, 1];
 
-    translationMatrix = eye(4); 
-    translationMatrix(1:4, 4) = -translationVector;
+
+    % Detector A was rotated by 45 degrees clockwise about z axis and the
+    % 4x4 homogeneous rotational matrix is a representation of that
+
     rotationMatrix_A = [cos(angle_A), -sin(angle_A), 0, 0;
                             sin(angle_A), cos(angle_A), 0, 0;
                             0, 0, 1, 0;
                             0, 0, 0, 1];
 
-    
+    % Detector B was rotated by 45 degrees counter clockwise about z axis and the
+    % 4x4 homogeneous rotational matrix is a representation of that
+
     rotationMatrix_B = [cos(angle_B), -sin(angle_B), 0, 0;
                             sin(angle_B), cos(angle_B), 0, 0;
                             0, 0, 1, 0;
                             0, 0, 0, 1];
+
+    % Since we are creating a homogeneous transformation matrix from the CK
+    % frame, which is the home frame "h", to the detectors' frames, which
+    % are destination frame "v". We multiply the homogeneous translation matrix
+    % with the homogeneous rotation matrix to get the homogeneous transformation matrix
 
     transformation_A = translationMatrix * rotationMatrix_A;
     transformation_B = translationMatrix * rotationMatrix_B;
--- XrayProjection.m
+++ XrayProjection.m
@@ -1,15 +1,17 @@
-function [pointA, pointB] = XrayProjection(pointCK)
+function [projection_A, projection_B] = XrayProjection(pointCK)
 % X-ray projection function to project a point in CK space onto the two imaging detectors (A and B).
 %
 % INPUT:
 %   pointCK: 3-element vector representing a point in CK space [x; y; z].
 
 % OUTPUT:
-%   pointA: 3-element vector [x_A; y_A; z_A] representing the coordinates
+%   projection_A: 3-element vector [x_A; y_A; z_A] representing the coordinates
 %           of the projected point in Detector A's image frame.
 %   pointB: 3-element vector [x_B; y_B; z_B] representing the coordinates
 %           of the projected p
 
+    % The following are given scalling factors that will assist in scalling
+    % the projected point appropriatly
 
     SDD = 200;  % Source-Detector Distance
     SAD = 100;  % Source-Axis Distance 
@@ -17,6 +19,7 @@ function [pointA, pointB] = XrayProjection(pointCK)
     % Firstly, we calculate the angles detector A and B were rotated by.
     % this woudl make it easier to calculate normal vectors for each in the
     % next step
+
     angle_A = deg2rad(45);
     angle_B = deg2rad(-45);
 
@@ -35,10 +38,47 @@ function [pointA, pointB] = XrayProjection(pointCK)
     normalVectorA = [sin(angle_A), cos(angle_A), 0];
     normalVectorB = [sin(angle_B), cos(angle_B), 0];
 
-    projection_A = dot(pointCK, normalVectorA) / norm(normal_A);
+    % The detectors' centre given in the asssignemnt's pdf file
+    detectorOrgin = [0, 0, 0];
+
+    % Here we are calling a helper functions that takes in the point in CK
+    % frame, the origin or centre of destination frame (really a point on
+    % the destination frame) and the normal vector of the detector and
+    % computes the projection from Home (3D CK frame) to destination (2D
+    % detector frame)
+
+    pointA = pointProjection(pointCK, detectorOrgin, normalVectorA);
+    pointB = pointProjection(pointCK, detectorOrgin, normalVectorB);
+
+    % Since coordinate z is typically zero or insignficant we extract the x
+    % and y coordinates and scale them using the Source-Detector Distance
+    % and Source-Axis Distance
     
-    projection_B = dot(pointCK, normalVectorB) / norm(normal_B);
+    projection_A = pointA(1:2) * (SDD/SAD);
+    projection_B = pointB(1:2) * (SDD/SAD);
 
+ 
+end
 
+function projection = pointProjection(Point, center, NormalVector)
+% Inputs:
+%   Point - 3D coordinates of the point to be projected.
+%   center - 3D coordinates of the plane's center.
+%   NormalVector - 3D normal vector of the plane.
+%
+% Output:
+%   projection - 3D coordinates of the projected point
 
+    % Calculate the direction vector from the center to the point
+        V = Point - center;
 
+        % Normalize the normal vector
+        N_normalized = NormalVector / norm(NormalVector);
+
+        % Calculate the dot product of V and the normalized normal vector
+        dotProduct = dot(V, N_normalized);
+
+        % Calculate the projected point
+        projection = Point - dotProduct * N_normalized;
+
+end
